baf51e75a04571e865334843aec7ae0f727b3b1a
Micah Henney <micah2004h@icloud.com>
2019-07-11 14:06:58 +0700

    index.js created and finished

---
+++ public/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+var tasks = [
+  "node ~/cssh/node/mandelbrot.js",
+  "node ~/cssh/node/mandelbrot.js"
+];
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  let temp = tasks.shift();
+  res.end(temp || "0" + "\n");
+  console.log(`Connection. Sent: ${temp || "0"}`)
+});
+
+server.listen(port);
